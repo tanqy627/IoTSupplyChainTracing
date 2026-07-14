113012069bb28b8b6638604576ef1c1373403d74
tanqy627 <148316289+tanqy627@users.noreply.github.com>
2026-07-14 19:37:00 +0800

    Delete tests directory

--- tests/test_device_names.py
+++
@@ -1,8 +0,0 @@
-from src.device_names import canonicalize_device_name
-
-
-def test_iot_lifecycle_device_names_are_canonicalized():
-    assert canonicalize_device_name("Camera_selfdata") == "Camera_IoTLifeCycle"
-    assert canonicalize_device_name("huawei_tv") == "huawei_tv_IoTLifeCycle"
-    assert canonicalize_device_name("Camera_YT") == "Camera_YT"
-    assert canonicalize_device_name("Camera_IoTLifeCycle") == "Camera_IoTLifeCycle"
--- tests/test_ps_dtw.py
+++
@@ -1,5 +0,0 @@
-from src.ps_dtw import ps_dtw_similarity
-
-def test_ps_dtw_identical():
-    seq = [60, 80, -1500, -300, 90]
-    assert ps_dtw_similarity(seq, seq) > 0.99
